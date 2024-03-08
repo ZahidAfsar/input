c94e0a1c2e95387b8d15157f666c644046d05d05
Zahid Afsar <zahidtempz@icloud.com>
2024-03-08 10:55:35 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Col, Row, Button } from 'react-bootstrap';
 import './App.css';
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+
+  interface Iperson {
+    id: number
+    name: string
+  }
+
+  // type person = {
+  //   id: number
+  //   name: string
+  // }
+
+  const [name, setName] = useState<string>('');
+
+  const [nameArray, setNameArray] = useState<Iperson[]>([]);
+
+  const [count, setCount] = useState<number>(0);
+
+  // for onChange events if the function is called outside of the onChange when using e (event parameter) we must give it a type
+  // const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   console.log(e);
+  // }
+
+  const handleSaveName = () => {
+    // we are using the spread operator to add one our object
+    setNameArray([...nameArray, {id: 0, name: name}])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Row>
+      <Col lg={12} md={9} sm={6} className='d-flex justify-content-center'>
+      <h1>Enter Name</h1>
+      </Col>
+      <Col lg={12} className='d-flex justify-content-center'>
+        {/* we are using anon function inside of onchange to set our name state variable to the value of e (our onchange press event) */}
+        {/* the value of our input field is always uptodate with out state variable */}
+        <input value={name} onChange={(e) => setName(e.target.value)} /> <Button onClick={handleSaveName} variant="dark">Save Name</Button>
+      </Col>
+      <Col lg={12} className='d-flex justify-content-center'>
+        <ul>
+          {/* .map takes in more than one parameter 1st is the value of the index, second is the index position */}
+          {nameArray.map((person, idx) => (
+            // when mapping through through arrays with jsx we need to have a key
+            <li key={idx}>{person.name}</li>
+          ))}
+        </ul>
+      </Col>
+      <Button onClick={() => setCount(count + 1)}>
+        +
+      </Button>
+      <p className='text-center' >{count}</p>
+    </Row>
   );
 }
 
